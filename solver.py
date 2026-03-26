7980448503f89e28bfdab1c7eac1fefc52186fc3
Yashvardhan <iamyashvardhansharma@gmail.com>
2026-03-26 19:02:52 +0530

    Implemented A* search in solver.py

---
+++ solver.py
@@ -0,0 +1,55 @@
+import heapq
+import time
+
+class AStarSolver:
+    def __init__(self, environment, heuristic_fn):
+        
+        self.env = environment
+        self.heuristic = heuristic_fn
+        
+        self.nodes_expanded = 0
+        self.max_frontier_size = 0
+        self.execution_time = 0
+
+    def solve(self, start_state):
+       
+        start_time = time.time()
+        
+        
+        initial_h = self.heuristic(start_state, self.env.goal_state)
+        frontier = [(initial_h, 0, start_state, [start_state])]
+        
+       
+        explored = {start_state: 0}
+        
+        self.nodes_expanded = 0
+        self.max_frontier_size = 1
+
+        while frontier:
+            
+            self.max_frontier_size = max(self.max_frontier_size, len(frontier))
+            
+            
+            f, g, current_state, path = heapq.heappop(frontier)
+            self.nodes_expanded += 1
+
+            
+            if self.env.goal_test(current_state):
+                self.execution_time = time.time() - start_time
+                return path, self.nodes_expanded, self.max_frontier_size, self.execution_time
+
+            
+            for action in self.env.get_actions(current_state):
+                neighbor = self.env.get_successor(current_state, action)
+                new_g = g + 1  
+                
+                
+                if neighbor not in explored or new_g < explored[neighbor]:
+                    explored[neighbor] = new_g
+                    h = self.heuristic(neighbor, self.env.goal_state)
+                    f_new = new_g + h
+                    
+                    heapq.heappush(frontier, (f_new, new_g, neighbor, path + [neighbor]))
+
+        self.execution_time = time.time() - start_time
+        return None, self.nodes_expanded, self.max_frontier_size, self.execution_time
