546b4cedc89b27c4b0ca4df18167d4e7858b5f5f
Vivek Panchotiya <vivekpanchotiya861@gmail.com>
2026-03-30 17:22:34 +0530

    Update solver.py

--- solver.py
+++ solver.py
@@ -147,7 +147,7 @@ class GreedyBestFirstSolver:
         start_time = time.time()
 
         initial_h = self.heuristic(start_state, self.env.goal_state)
-        frontier = [(intial_h, start_state, [start_state])]
+        frontier = [(initial_h, start_state, [start_state])]
 
         explored = set([start_state])
         self.nodes_expanded = 0
